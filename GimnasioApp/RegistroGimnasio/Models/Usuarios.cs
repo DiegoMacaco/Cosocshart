02dcd6fe586bc82e63dfa6eb5c916f6602a9da52
DiegoMacaco <diego.guisbert.62103@usalesiana.edu.bo>
2025-09-01 02:38:44 -0400

    Refactor:Refactorizabndo el codigo

--- GimnasioApp/RegistroGimnasio/Models/Usuarios.cs
+++ GimnasioApp/RegistroGimnasio/Models/Usuarios.cs
@@ -1,14 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RegistroGimnasio.Models
 {
     public class Usuario
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
 
-        public required string Nombre { get; set; }
-        public required string Telefono { get; set; }
-        public required string Identificacion { get; set; }
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [Phone(ErrorMessage = "Debe ser un número de teléfono válido")]
+        public string Telefono { get; set; }
 
+        [Required(ErrorMessage = "La identificación es obligatoria")]
+        public string Identificacion { get; set; }
+
+        [Required(ErrorMessage = "El período es obligatorio")]
+        [Range(1, 36, ErrorMessage = "El período debe estar entre 1 y 36 meses")]
         public int PeriodoMeses { get; set; }
     }
-
 }
